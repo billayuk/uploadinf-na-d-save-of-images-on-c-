144e895320e4bcb146064fb8f63144a9fed9acae
billayuk <57730646+billayuk@users.noreply.github.com>
2021-10-14 17:32:04 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace uploadandSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.uploadImageFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.savedOrUnsaved = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(74, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 173);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // uploadImageFile
+            // 
+            this.uploadImageFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uploadImageFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.uploadImageFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uploadImageFile.Location = new System.Drawing.Point(634, 89);
+            this.uploadImageFile.Name = "uploadImageFile";
+            this.uploadImageFile.Size = new System.Drawing.Size(164, 40);
+            this.uploadImageFile.TabIndex = 1;
+            this.uploadImageFile.Text = "Upload Image File";
+            this.uploadImageFile.UseVisualStyleBackColor = true;
+            this.uploadImageFile.Click += new System.EventHandler(this.uploadImageFile_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(645, 202);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(164, 40);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // savedOrUnsaved
+            // 
+            this.savedOrUnsaved.AutoSize = true;
+            this.savedOrUnsaved.Location = new System.Drawing.Point(111, 361);
+            this.savedOrUnsaved.Name = "savedOrUnsaved";
+            this.savedOrUnsaved.Size = new System.Drawing.Size(0, 17);
+            this.savedOrUnsaved.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.savedOrUnsaved);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.uploadImageFile);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Upload and save image";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button uploadImageFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label savedOrUnsaved;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uploadandSave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void uploadImageFile_Click(object sender, EventArgs e)
+        {
+
+            // pictureBox pictureBox = new pictureBox();
+            try
+            {
+
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+
+                    textBox1.Text = ofd.FileName;
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            //FileDialog.Equals(textBox1.Text,path.combind("C:\Users\User\OneDrive - University of Derby\Documents\2020-2021 university of derby\year 1\programming II\exercises and reading note and solutions\my codes\uploadandSave\uploadandSave\images"))             //.copy(textBox1)
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+
+           
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
